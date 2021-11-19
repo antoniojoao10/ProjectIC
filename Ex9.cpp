91a61fa263c2e05bc63f22cc057dc698e4f60d81
Pedro Coutinho <pmacoutinho@ua.pt>
2021-11-19 22:02:21 +0000

    Better Ex9

--- Ex9.cpp
+++ Ex9.cpp
@@ -4,19 +4,31 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
+inline uchar reduceVal(const uchar val) {
+    if (val < 64) 
+        return 0;
+    
+    if (val < 128) 
+        return 64;
+    
+    return 255;
+}
+
 int main(int argc, char* argv[]) {   
     cv::Mat input = cv::imread(argv[1]);
 
-    int nl = input.rows;                    
-    int nc = input.cols * input.channels(); 
-
-    for (int j = 0; j < nl; j++) {
-        uchar* data = input.ptr<uchar>(j);
-
-        for (int i = 0; i < nc; i++) 
-            data[i] = data[i] / 64 * 64 + 64 / 2;
+    uchar* pixels = input.data;
+    
+    for (int i = 0; i < input.rows; i++) {
+        for (int j = 0; j < input.cols; j++) {
+            const int p = i * input.cols * 3 + j * 3;
+    
+            pixels[p + 2] = reduceVal(pixels[p + 2]); // Red
+            pixels[p + 1] = reduceVal(pixels[p + 1]); // Green
+            pixels[p + 0] = reduceVal(pixels[p + 0]); // Blue
+        }
     }
- 
+
     cv::imwrite("new_image.jpg", input);   
     cv::waitKey(0);
 
